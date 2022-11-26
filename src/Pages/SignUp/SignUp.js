4dfab11d9f689fb3e167ff16cc88efe14a4c754c
Amirulmath <amirulmath760@gmail.com>
2022-11-26 17:58:11 +0600

    React Hook Form used on SignUp page

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,50 +1,71 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const handleSignUp = data => {
+        console.log(data);
+    }
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col lg:flex-row">
                     <div className="text-center lg:text-left">
-                        <h1 className="text-5xl font-bold">Sign Up now!</h1>
-                        <p className="py-6">If you are a new user, you must first create an account and sign up.</p>
+                        <h1 className="text-5xl font-bold">Login now!</h1>
+                        <p className="py-6">If you want to access our services, you need to login first.</p>
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Your Name</span>
-                                </label>
-                                <input type="text" placeholder="name" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Specialty</span>
-                                </label>
-                                <select className="select select-bordered w-full max-w-xs">
-                                    <option>Buyer</option>
-                                    <option>Seller</option>
-                                </select>
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Sign Up</button>
-                            </div>
-                            <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
-                            <div className="divider">OR</div>
-                            <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                            <form onSubmit={handleSubmit(handleSignUp)}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Your Name</span>
+                                    </label>
+                                    <input type="text" {...register("name", {
+                                        required: "Name is Required"
+                                    })} placeholder="name" className="input input-bordered" />
+                                    {errors.name && <p className='text-red-500'>{errors.name.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" {...register("email", {
+                                        required: "Email Address is required"
+                                    })} placeholder="email" className="input input-bordered" />
+                                    {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password"
+                                        {...register("password", {
+                                            required: "Password is required",
+                                            minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                                        })}
+                                        placeholder="password" className="input input-bordered" />
+                                    <label className="label"> <span className="label-text">Forget Password?</span></label>
+                                    {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Specialty</span>
+                                    </label>
+                                    <select {...register("specialty", { required: true })} className="select select-bordered w-full max-w-xs">
+                                        <option value="Buyer">Buyer</option>
+                                        <option value="Seller">Seller</option>
+                                    </select>
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input className='btn btn-primary w-full' value="Sign Up" type="submit" />
+                                </div>
+                                <p>Already have an account <Link className='text-secondary' to="/login">Please Login</Link></p>
+                                <div className="divider">OR</div>
+                                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                            </form>
                         </div>
                     </div>
                 </div>
