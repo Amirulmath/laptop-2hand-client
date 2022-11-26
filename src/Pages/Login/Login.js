8793a0791e0ea604020398c15530d26819775d24
Amirulmath <amirulmath760@gmail.com>
2022-11-26 17:19:10 +0600

    React Hook Form used on Login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const handleLogin = data => {
+        console.log(data);
+    }
+
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
@@ -12,24 +18,36 @@ const Login = () => {
                     </div>
                     <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
                         <div className="card-body">
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Email</span>
-                                </label>
-                                <input type="text" placeholder="email" className="input input-bordered" />
-                            </div>
-                            <div className="form-control">
-                                <label className="label">
-                                    <span className="label-text">Password</span>
-                                </label>
-                                <input type="text" placeholder="password" className="input input-bordered" />
-                            </div>
-                            <div className="form-control mt-6">
-                                <button className="btn btn-primary">Login</button>
-                            </div>
-                            <p>New to Laptop2Hand <Link className='text-secondary' to="/signup">Create new Account</Link></p>
-                            <div className="divider">OR</div>
-                            <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                            <form onSubmit={handleSubmit(handleLogin)}>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="text" {...register("email", {
+                                        required: "Email Address is required"
+                                    })} placeholder="email" className="input input-bordered" />
+                                    {errors.email && <p className='text-red-600'>{errors.email?.message}</p>}
+                                </div>
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password"
+                                        {...register("password", {
+                                            required: "Password is required",
+                                            minLength: { value: 6, message: 'Password must be 6 characters or longer' }
+                                        })}
+                                        placeholder="password" className="input input-bordered" />
+                                    <label className="label"> <span className="label-text">Forget Password?</span></label>
+                                    {errors.password && <p className='text-red-600'>{errors.password?.message}</p>}
+                                </div>
+                                <div className="form-control mt-6">
+                                    <input className='btn btn-primary w-full' value="Login" type="submit" />
+                                </div>
+                                <p>New to Laptop2Hand <Link className='text-secondary' to="/signup">Create new Account</Link></p>
+                                <div className="divider">OR</div>
+                                <button className='btn btn-outline w-full'>CONTINUE WITH GOOGLE</button>
+                            </form>
                         </div>
                     </div>
                 </div>
